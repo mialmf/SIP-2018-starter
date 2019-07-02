d6445e60ec56c90b85f9e5a3fb9729abb2fd2b2b
Your Name <you@example.com>
2019-07-02 02:48:55 -0700

    amazing data project

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -1,7 +1,5 @@
 '''
-In this project, you will visualize the feelings and language used in a set of
-Tweets. This starter code loads the appropriate libraries and the Twitter data you'll
-need!
+In this program, we store the polarities and subjectivities of all the tweets.
 '''
 
 import json
@@ -13,8 +11,43 @@ tweetFile = open("TwitterData/tweets_small.json", "r")
 tweetData = json.load(tweetFile)
 tweetFile.close()
 
-# Continue your program below! 
+#Create a Sentiment List
+data = '{tweet_small.json}'
+json_string = json.dumps(data)
+polarityLIst = []
+subjectivityList = []
+tweettext = []
+tweetstring = ''
 
-# Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+#Get Sentiment Data
+for tweet in tweetData:
+    tweetstring += tweet['text']
+    #print(tweetstring)
+
+for tweet in tweetData:
+	tweetblob = TextBlob(tweet["text"])
+	polarityLIst.append(tweetblob.polarity)
+
+for tweet in tweetData:
+    tweetblob = TextBlob(tweet['text'])
+    subjectivityList.append(tweetblob.subjectivity)
+tweetBlob = TextBlob(tweetstring)
+word_dict = {}
+
+for word in tweetBlob.words:
+    word_dict[word.lower()] = tweetBlob.word_counts[word.lower()]
+
+print(word_dict)
+
+my_list = polarityLIst
+o_list = subjectivityList
+#print(sum(my_list)/len(my_list))
+#print(sum(o_list)/len(o_list))
+
+#plt.hist(my_list, bins=[-1, -0.9, -0.8, -0.7, -0.6, -0.5, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+#plt.xlabel('Values')
+#plt.ylabel('Number of Items')
+#plt.title('Histogram of Numbers')
+#plt.axis([-1.0, 1.0, 0, 100])
+#plt.grid(True)
+#plt.show()
